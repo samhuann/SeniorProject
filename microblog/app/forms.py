36bbdd8750e5fb5ac027e06f0372a827cb7c4b63
samhuann <samuelhuang0108@gmail.com>
2024-04-24 00:41:56 -0700

    With Login (display excel broken)

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,32 +1,34 @@
-from datetime import datetime, timezone
-from typing import Optional
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 import sqlalchemy as sa
-import sqlalchemy.orm as so
 from app import db
+from app.models import User
 
-class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
 
-class Post(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
 
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,30 +1,68 @@
+from urllib.parse import urlsplit
 from flask import render_template, flash, redirect, url_for, request
-import os
+from flask_login import login_user, logout_user, current_user, login_required
+import sqlalchemy as sa
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 import pandas as pd
-from app import app
-from app.forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    return render_template('index.html', title='Home')
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
-@app.route('/parse', methods=['POST'])
-def submit_form():
-    dsc = request.form.get('description')
-    gender = request.form.get('gender')
-    dsc = dsc.upper()
-    # Process the form data
-    return render_template("result.html",
-                               dsc=dsc,
-                               gender=gender)
-@app.route('/upload', methods=['POST'])
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 def upload_file():
     if 'file' not in request.files:
@@ -41,4 +79,4 @@ def upload_file():
         # Now you can use the DataFrame 'df' to work with the uploaded Excel data
         # For example, print the first few rows of the DataFrame
         print(df.head())
-        return render_template('result.html', filename=file.filename, tables=[df.to_html(classes='data')], titles=df.columns.values)
+        return render_template('index.html', filename=file.filename, tables=[df.to_html(classes='data')], titles=df.columns.values)
